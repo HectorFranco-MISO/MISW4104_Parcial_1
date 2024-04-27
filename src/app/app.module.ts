bc2a88f9b5af49a364b8c5bf2ccee62d0f38ac90
HectorFranco-MISO <h.franco@uniandes.edu.co>
2024-04-27 13:36:54 -0500

    Añadir el módulo a app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CafesModule } from './cafes/cafes.module';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CafesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
